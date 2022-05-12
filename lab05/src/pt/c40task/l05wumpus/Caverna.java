5655b9512f101080b4a12d3b35f962122241f97d
luisacolafati <luisacolafati@gmail.com>
2022-05-12 14:31:31 -0300

    adding methods in classes Caverna and Mochila

--- lab05/src/pt/c40task/l05wumpus/Caverna.java
+++ lab05/src/pt/c40task/l05wumpus/Caverna.java
@@ -11,6 +11,81 @@ package pt.c40task.l05wumpus;
 
 public class Caverna {
 	
-	Sala[][] caverna = new Sala[3][3];
+	private int DIMENSAO_CAVERNA = 3;
+	private Sala[][] caverna;
+	private boolean[][] emptyRooms;
+
+	public Caverna() {
+		this.setCaverna();
+		this.setEmptyRooms();
+	}
+
+	private void setCaverna () {
+		this.caverna = new Sala[this.DIMENSAO_CAVERNA][this.DIMENSAO_CAVERNA];
+		for (int i = 0; i < this.DIMENSAO_CAVERNA; i++) {
+			for (int j = 0; j < this.DIMENSAO_CAVERNA; j++) {
+				caverna[i][j] = null;
+			}
+		}
+	}
+
+	private void setEmptyRooms () {
+		this.emptyRooms = new boolean[this.DIMENSAO_CAVERNA][this.DIMENSAO_CAVERNA];
+		for (int i = 0; i < this.DIMENSAO_CAVERNA; i++) {
+			for (int j = 0; j < this.DIMENSAO_CAVERNA; j++) {
+				emptyRooms[i][j] = true;
+			}
+		}
+	}
+
+	private void insereSala (Sala sala) {
+		if (this.caverna.length == this.DIMENSAO_CAVERNA * this.DIMENSAO_CAVERNA) {
+			throw new Error("Caverna cheia! Nao eh possivel inserir mais salas nela")
+		}
+		for (int i = 0; i < this.DIMENSAO_CAVERNA; i++) {
+			for (int j = 0; j < this.DIMENSAO_CAVERNA; j++) {
+				if (emptyRooms[i][j]) {
+					caverna[i][j] = sala;
+					emptyRooms[i][j] = false;
+					return;
+				}
+			}
+		}
+	}
+
+	private void removeSala(int idSala) {
+		for (int i = 0; i < this.DIMENSAO_CAVERNA; i++) {
+			for (int j = 0; j < this.DIMENSAO_CAVERNA; j++) {
+				if (caverna[i][j].id == idSala) {
+					caverna[i][j] = null;
+					emptyRooms[i][j] = true;
+					return;
+				}
+			}
+		}
+		throw new Error("Nehuma sala com id " + idSala + " foi encontrada na caverna");
+	}
+
+	private void insereComponenteNaSala(int idSala, Componente comp) {
+		for (int i = 0; i < this.DIMENSAO_CAVERNA; i++) {
+			for (int j = 0; j < this.DIMENSAO_CAVERNA; j++) {
+				if (caverna[i][j].id == idSala) {
+					caverna[i][j].insert(comp);
+					return;
+				}
+			}
+		}
+	}
+
+	private void removeComponenteDaSala(int idSala, Componente comp) {
+		for (int i = 0; i < this.DIMENSAO_CAVERNA; i++) {
+			for (int j = 0; j < this.DIMENSAO_CAVERNA; j++) {
+				if (caverna[i][j].id == idSala) {
+					caverna[i][j].remove(comp.getComponente());
+					return;
+				}
+			}
+		}
+	}
 	
 }
--- lab05/src/pt/c40task/l05wumpus/Mochila.java
+++ lab05/src/pt/c40task/l05wumpus/Mochila.java
@@ -7,15 +7,23 @@ package pt.c40task.l05wumpus;
  *
  */
 
-public class Mochila extends Componente{
+public class Mochila extends Componente {
 
-	Componente[] mochila;
+	private Componente[] mochila;
+	private int posicaoUltimoComponente;
 	
 	/**
-	 * Monta mochila com temMoch espaços e recebe um array de numFlechas flechas, presumindo tamMoch >= numFlechas + 2;
+	 * Monta mochila com tamMochila espaços e recebe um array de numFlechas flechas, presumindo tamMoch >= numFlechas + 2;
 	 */
-	public Mochila(Componente[] aljava, int tamMoch, int numFlechas) {
-		mochila = new Componente[tamMoch];
+	public Mochila(Componente[] aljava, int tamMochila, int numFlechas) {
+		this.setMochila(aljava, tamMochila, numFlechas);
+		this.posicaoUltimoComponente = -1;
+	}
+
+	private void setMochila (Componente[] aljava, int tamMochila, int numFlechas) {
+		int tamanho = tamMochila >= (numFlechas + 2) ? tamMochila : (numFlechas + 2);
+		this.mochila = new Componente[tamanho];
+
 		for (int i = 0; i < numFlechas; i++) {
 			mochila[i] = aljava[i];
 			aljava[i] = null;
@@ -26,8 +34,38 @@ public class Mochila extends Componente{
 	 * Insere comp na posição solicitada na mochila.
 	 * E se herói solicitar pra inserir em posição já preenchida?
 	 */
-	public void insert(Componente comp, int pos) {
-		
+	public int insere(Componente comp) {
+		if (this.posicaoUltimoComponente == this.mochila.length) {
+			throw new Error("Mochila cheia! Nao eh possivel guardar o componente " + comp.getComponente());
+		}
+		this.posicaoUltimoComponente++;
+		this.mochila[this.posicaoUltimoComponente] = comp;
+		return this.posicaoUltimoComponente;
+	}
+
+	/**
+	 * Remove o componente guardado numa posicao pos da mochila e,
+	 * a partir dessa posição, copia os componentes seguintes que
+	 * posuem posição p para uma posição (p - 1)
+	 */
+	private void reordenaMochilaAPartirDaPosicao(int pos) {
+		for (int i = pos; i < (this.mochila.length - 1); i++) {
+			this.mochila[i] = this.mochila[i+1];
+		}
+		this.mochila[this.posicaoUltimoComponente] = null;
+		this.posicaoUltimoComponente--;
+	}
+
+	/**
+	 * Remove um comp da mochila
+	 */
+	public void remove(Componente comp) {
+		for (int i = 0; i < this.mochila.length; i++) {
+			if (this.mochila[i].id == comp.id) {
+				this.reordenaMochilaAPartirDaPosicao(i);
+				break;
+			}
+		}
 	}
 	
 	@Override
